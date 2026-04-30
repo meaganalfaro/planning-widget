b6fec4662acabd9659793e0f09e89d713ee473c5
Meagan Alfaro <meaganalfaro1@gmail.com>
2026-04-30 14:59:49 -0400

    Added the main.jsx and incorporated the window tabs

---
+++ src/themes/themes.js
@@ -0,0 +1,38 @@
+export const themes = {
+    dark: {
+        primary: '#361800',
+        secondary: '#5E4632',
+        accent: '#b5e6f0', // try this #001e36
+        letters: '#fff',
+        title: '#F5FBFF',
+        design: '#5e4632'
+    },
+
+    light: {
+        primary: '#ff92c4',
+        secondary: '#ffa7cf',
+        accent: '#f2e8a8', // try this #92ffcd
+        letters: '#000',
+        title: '#302e21',
+        design: '#ffd3e7'
+    },
+
+    cool: {
+        primary: '#b5e6f0',
+        secondary: '#c3ebf3',
+        accent: '#ff92c4', // try this #f0bfb5
+        letters: '#000',
+        title: '#302e21',
+        design: '#e1f5f9'
+
+    },
+
+    warm: {
+        primary: '#f2e8a8',
+        secondary: '#f4ecb9',
+        accent: '#ff92c4', // or try: '#a8b2f2
+        letters: '#000',
+        title: '#302e21',
+    }
+
+}
